c639f841f237ef0104519fec2c956c80d633dd51
xion <xiongxiao1012@outlook.com>
2021-11-21 17:07:51 +0800

    init simple react

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <>
+      <h1>简单的react + typescript写法</h1>
+      <h2>只包含必要可以跑起来的功</h2>
+    </>
+  );
+};
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const pkgs = require('./package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const WEBPACK_SERVE = process.env.WEBPACK_SERVE;
+const join = path.join;
+const mode = WEBPACK_SERVE ? 'development' : 'prodution';
+
+const common = {
+  target: 'web',
+  mode: mode,
+  entry: './src/index.tsx',
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  output: {
+    publicPath: `/`,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    hot: true,
+    allowedHosts: 'all',
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    port: 8080,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      '@': join(__dirname, 'src'),
+    },
+  },
+  stats: 'errors-warnings',
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|svg|gif|bmp)/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true, // 开发环境下开启 babel 缓存
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      cache: true,
+      version: pkgs.version,
+    }),
+  ],
+};
+
+module.exports = common
